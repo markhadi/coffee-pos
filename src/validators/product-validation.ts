7deda0fe9f508bd4c6b66591fccc19deb50b7143
Markhadi <markhadi@protonmail.com>
2024-10-30 07:11:00 +0800

    fix(product): synchronize search parameter naming across layers

--- src/validators/product-validation.ts
+++ src/validators/product-validation.ts
@@ -1,28 +1,24 @@
-import { z } from "zod";
-import {
-  minimumString,
-  maximumString,
-  nonnegative,
-} from "./validation-message";
+import { z } from 'zod';
+import { minimumString, maximumString, nonnegative } from './validation-message';
 
 export class ProductValidation {
   static readonly CREATE = z.object({
     code: z
       .string()
-      .min(1, { message: minimumString(1, "Code") })
-      .max(10, { message: maximumString(10, "Code") }),
+      .min(1, { message: minimumString(1, 'Code') })
+      .max(10, { message: maximumString(10, 'Code') }),
     name: z
       .string()
-      .min(1, { message: minimumString(1, "Name") })
-      .max(100, { message: maximumString(100, "Name") }),
-    stock: z.number().nonnegative({ message: nonnegative("Stock") }),
-    price: z.number().nonnegative({ message: nonnegative("Price") }),
+      .min(1, { message: minimumString(1, 'Name') })
+      .max(100, { message: maximumString(100, 'Name') }),
+    stock: z.number().nonnegative({ message: nonnegative('Stock') }),
+    price: z.number().nonnegative({ message: nonnegative('Price') }),
     category_id: z.number(),
   });
   static readonly SEARCH = z.object({
-    name: z
+    search: z
       .string()
-      .max(100, { message: maximumString(100, "Name") })
+      .max(100, { message: maximumString(100, 'Name') })
       .optional(),
     size: z.number().optional(),
     cursor: z.number().optional(),
@@ -31,14 +27,14 @@ export class ProductValidation {
     id: z.number(),
     code: z
       .string()
-      .min(1, { message: minimumString(1, "Code") })
-      .max(10, { message: maximumString(10, "Code") }),
+      .min(1, { message: minimumString(1, 'Code') })
+      .max(10, { message: maximumString(10, 'Code') }),
     name: z
       .string()
-      .min(1, { message: minimumString(1, "Name") })
-      .max(100, { message: maximumString(100, "Name") }),
-    stock: z.number().nonnegative({ message: nonnegative("Stock") }),
-    price: z.number().nonnegative({ message: nonnegative("Price") }),
+      .min(1, { message: minimumString(1, 'Name') })
+      .max(100, { message: maximumString(100, 'Name') }),
+    stock: z.number().nonnegative({ message: nonnegative('Stock') }),
+    price: z.number().nonnegative({ message: nonnegative('Price') }),
     category_id: z.number(),
   });
 }
